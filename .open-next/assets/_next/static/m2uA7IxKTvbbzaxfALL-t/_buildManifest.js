8d4ad3c1ec55d42b3e4780b0ebf4a0189c56478b
Reidar Djupvik <reidar@djupvik.digital>
2023-04-21 23:16:00 +0100

    Add updated output files after deploying to prod

--- .open-next/assets/_next/static/m2uA7IxKTvbbzaxfALL-t/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1b55222842c4b5e4.css","static/chunks/pages/index-11206a76d9f4017f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
